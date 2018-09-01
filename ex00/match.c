800410d5a258b01809a911ef2752ff703b0efd13
Dancheek <dancheek22@gmail.com>
2018-09-01 11:09:19 +0300

    removed comments

--- ex00/match.c
+++ ex00/match.c
@@ -3,7 +3,6 @@
 
 int	match(char *s1, char *s2)
 {
-	//printf("%s, %s\n", s1, s2);
 	if (*s2 == 0)
 		return (*s1 == 0);
 	else if (*s2 == '*')
@@ -20,29 +19,6 @@ int	match(char *s1, char *s2)
 	else
 		return (match(s1 + 1, s2 + 1));
 }
-
-
-/*int match1(const char *pattern, const char *candidate, int p, int c)
-{
-	printf("%s, %s | ", &candidate[c], &pattern[p]);
-	if (pattern[p] == '\0')
-	{
-		return (candidate[c] == '\0');
-	} 
-	else if (pattern[p] == '*')
-	{
-		while (candidate[c] != '\0')
-		{
-			if (match1(pattern, candidate, p+1, c++))
-				return (1);
-		}
-		return (match1(pattern, candidate, p+1, c));
-	}
-	else if (pattern[p] != '?' && pattern[p] != candidate[c])
-		return (0);
-	else
-		return (match1(pattern, candidate, p+1, c+1));
-}*/
 
 int	main(int argc, char **argv)
 {
